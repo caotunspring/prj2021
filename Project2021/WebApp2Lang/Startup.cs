d71dba0c6a914293330ce728a3dfa260606b7a9a
CaotunSpring <CaotunSpring@outlook.com>
2021-03-06 18:42:48 +0800

    confirmed that cookie not for utf8

--- Project2021/WebApp2Lang/Startup.cs
+++ Project2021/WebApp2Lang/Startup.cs
@@ -1,4 +1,4 @@
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -25,10 +25,27 @@ namespace WebApp2Lang
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
+            services.AddLocalization(opt => opt.ResourcesPath = "Resources");
+
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
         }
+
+        private RequestLocalizationOptions GetLocalizationOptions()
+        {
+            var cultures = Configuration.GetSection("Clutures")
+                .GetChildren().ToDictionary(x => x.Key, x => x.Value);
+            var supportedCultures = cultures.Keys.ToArray();
+
+            var localizationOptions = new RequestLocalizationOptions()
+                .AddSupportedCultures(supportedCultures)
+                .AddSupportedUICultures(supportedCultures);
+            return localizationOptions;
+        }
+
+
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -44,10 +61,17 @@ namespace WebApp2Lang
 
             app.UseStaticFiles();
 
+
+            app.UseRequestLocalization(GetLocalizationOptions());
+
+
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
+
+                endpoints.MapControllers();
+
                 endpoints.MapBlazorHub();
                 endpoints.MapFallbackToPage("/_Host");
             });
